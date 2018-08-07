8ea8f912f3b6c80386fcc2f9014a8b20be00a95c
mattbrailsford <me@mattbrailsford.com>
2018-08-07 09:19:47 +0100

    Added qunit, still need to write unit tests

---
+++ tests/units/core.js
@@ -0,0 +1,23 @@
+// Helpers
+const createHtml = (html) => {
+    var template = document.createElement('template');
+    template.innerHTML = html.trim();
+    return template.content.firstChild;
+}
+
+// Unit tests
+QUnit.test("Can parse value attribute", function (assert) {
+
+    // Arrange
+    let el = createHtml(`<div id="data">
+        <div data-dtn-value="name">John</div>
+    </div>`);
+
+    // Act
+    let obj = DATON.parse(el);
+
+    // Assert
+    assert.ok(obj.hasOwnProperty('name'), "Has matching object key");
+    assert.equal(obj.name, "John", "Object value matches template value");
+
+});
